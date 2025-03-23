61f13d1e65766952f82a6ba1f2b1ad0248157fbf
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-03-23 12:17:53 +0700

    .

--- src/popup.js
+++ src/popup.js
@@ -1,22 +1,44 @@
 // chrome.tabs.create({ url: chrome.runtime.getURL("common/html/option.html") });
+
+const mesErrorConnect = `Unable to connect to Facebook. Please refresh the Messenger page and try again.`;
+
 document.addEventListener("DOMContentLoaded", async () => {
    const input = document.querySelector(".input-focus_chat");
+   const statusEl = document.querySelector(".status-focus_chat");
    const startBtn = document.querySelector(".button-start-hide_chat");
    const stopBtn = document.querySelector(".button-stop-hide_chat");
+
+   input.addEventListener("blur", () => {
+      const keyword = input.value.trim();
+      if (keyword === ``) return;
+      chrome.storage.local.set({ chatFilterKeyword: keyword });
+   });
 
    const { chatFilterKeyword } = await chrome.storage.local.get("chatFilterKeyword");
    if (chatFilterKeyword) {
       input.value = chatFilterKeyword;
    }
 
-   sendEvent({ action: "get-init-status" }, (res) => {
-      console.log({ res });
-      if (res) {
-         updateButtonState(`stop`, startBtn, stopBtn);
-      } else {
-         updateButtonState(`start`, startBtn, stopBtn);
+   sendEvent(
+      { action: "get-init-status" },
+      (res) => {
+         console.log({ res });
+         statusEl.textContent = "";
+         if (res) {
+            updateButtonState(`stop`, startBtn, stopBtn);
+         } else {
+            updateButtonState(`start`, startBtn, stopBtn);
+         }
+      },
+      (err, payload) => {
+         if (payload.action === "get-init-status") {
+            if (statusEl) {
+               statusEl.textContent = mesErrorConnect;
+               statusEl.style.color = "red";
+            }
+         }
       }
-   });
+   );
 
    startBtn.addEventListener("click", async () => {
       const keyword = input.value.trim();
@@ -55,14 +77,15 @@ document.addEventListener("DOMContentLoaded", async () => {
    });
 
    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (statusEl) statusEl.textContent = "";
       if (message.action === "filter-restarted") {
-         if(message.status) {
+         if (message.status) {
             updateButtonState("stop", startBtn, stopBtn);
          } else {
             updateButtonState("start", startBtn, stopBtn);
          }
       }
-    });
+   });
 });
 
 function updateButtonState(active, startBtn, stopBtn) {
@@ -75,13 +98,13 @@ function updateButtonState(active, startBtn, stopBtn) {
    }
 }
 
-function sendEvent(payload, cb) {
+function sendEvent(payload, cb, cbError) {
    chrome.tabs.query({ url: "https://www.messenger.com/*" }, (tabs) => {
       tabs.forEach((tab) => {
          chrome.tabs.sendMessage(tab.id, payload, (response) => {
             if (chrome.runtime.lastError) {
-               console.error("❌ Lỗi gửi message:", chrome.runtime.lastError.message);
-               // alert("Not content script.");
+               console.log("❌ Lỗi gửi message:", chrome.runtime.lastError.message);
+               if (typeof cbError === `function`) cbError(chrome.runtime.lastError, payload);
                return;
             }
             cb(response);
